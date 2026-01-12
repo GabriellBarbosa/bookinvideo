7f01a0e8bcda258f8e46181a32719d6075aa2128
GabriellBarbosa <bielbarbosa900@gmail.com>
2026-01-12 11:55:51 -0300

    feat(api): Throw Not Found Exception

--- apps/api/src/modules/course/course.controller.ts
+++ apps/api/src/modules/course/course.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { CourseService } from './course.service';
 
 @Controller('course')
@@ -7,11 +7,11 @@ export class CourseController {
 
   @Get('/course-structure/:slug')
   async getCourseStructure(@Param('slug') slug: string) {
-    console.log(slug);
-
     const course = await this.courseService.getCourseStructure(slug);
 
-    console.log(course);
+    if (!course) {
+      throw new NotFoundException();
+    }
 
     return course;
   }
