2e5aa9841426a7ddd9fb8b26a816d2f875cc5ab0
GabriellBarbosa <bielbarbosa900@gmail.com>
2026-01-31 12:53:25 -0300

    feat(api): Allow all origins

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.ENVIRONMENT === 'development') {
-    app.enableCors({
-      origin: ['http://localhost:3000'],
-      credentials: true,
-    });
-  }
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
